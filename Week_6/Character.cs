6192def9ca03e680a94628198b0c06dc25253636
duygu <duygu.bayburtli571@gmail.com>
2022-10-31 15:34:54 +0300

    hello

---
+++ Week_6/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_6
+{
+    public  abstract class Character
+    {
+        public abstract string Name { get; set; } 
+        public abstract int Health { get; set; } 
+        public abstract int Stamina { get; set; }
+        public abstract int Damage { get; set; }
+        public abstract void Attack (ref int enemyHealth);
+        public abstract void Wait ();
+    }
+}
---
+++ Week_6/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Week_6
+{
+    public class Program
+    {
+        private static int kHealth;
+
+        public static void Main()
+        {
+            Knight knigth = new Knight("Sir Knigth");
+            Enemy enemy = new Enemy();
+
+            var kHealth = knigth.Health;
+            var eHealth = enemy.Health;
+
+
+            while (kHealth > 0)
+            {
+                knigth.Attack (ref eHealth);
+                enemy.Attack (ref kHealth);
+                Console.WriteLine(
+                   $"-----------------\n" +
+                   $"{enemy.Name} Health :{eHealth}\n" +
+                   $"{knigth.Name} :{kHealth}");
+
+            }
+
+
+        }
+    }
+}
